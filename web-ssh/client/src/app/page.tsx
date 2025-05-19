2dcd49c5f5e058730343e3419749bf328fc1a935
Larry Banks <larrykbanks21@gmail.com>
2025-05-19 19:02:15 -0400

    moved back to root page for now, makes development easier

--- web-ssh/client/src/app/page.tsx
+++ web-ssh/client/src/app/page.tsx
@@ -1,6 +1,42 @@
+"use client";
+import Terminal from "../components/Terminal";
+import { useState } from "react";
+export default function session() {
+    // first need to enter a destination
+    // then need to establish a connection
+    const [isLoading, setIsLoading] = useState(false);
+    const [destination, setDestination] = useState("");
+    const [sessionStarted, setSessionStarted] = useState(false);
 
-export default function Home() {
-  return (
-    <div>"Home Page"</div>
-  );
+    function startSession() {
+        if (destination === "") {
+            console.log(`destination cannot be empty.`);
+            // render an error here
+            return;
+        }
+        setIsLoading(true);
+        console.log(`starting ssh connection to ${destination}`);
+        setSessionStarted(true);
+        setIsLoading(false);
+    }
+
+    if (isLoading) {
+        return (<div>Loading!!!</div>);
+    }
+
+    if (sessionStarted) {
+        return (
+            <div><Terminal destination={destination} /></div>
+        );
+    }
+
+    return (
+        <div>
+            <label>Hostname</label>
+            <input type="text" onChange={(e) => { setDestination(e.target.value) }}></input>
+            <button onClick={() => {
+                startSession();
+            }}>Submit</button>
+        </div>
+    )
 }
