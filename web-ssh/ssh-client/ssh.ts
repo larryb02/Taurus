035244c2e6a3618c2cff834f262e2bcc39b200a0
Larry Banks <larrykbanks21@gmail.com>
2025-05-30 15:55:00 -0400

    added hack to specify port for now & added proper access specifiers to members and methods

--- web-ssh/ssh-client/ssh.ts
+++ web-ssh/ssh-client/ssh.ts
@@ -1,31 +1,36 @@
 import { logger } from './logger/logger.js';
 
 export class SSHConn {
-    user: string = "";
-    hostname: string = "";
-    password: string = "";
-    command: string [] = [];
+    private _user: string = "";
+    private _hostname: string = "";
+    private _command: string [] = [];
 
     constructor(
         user: string,
         hostname: string,
     ) {
-        this.user = user;
-        this.hostname = hostname;
-        this.command = this.build();
+        this._user = user;
+        this._hostname = hostname;
+        this._command = this.build();
     } 
 
-    build (): string [] {
+    private build (): string [] {
         let args: string [] = [];
-        const target = `${this.user}@${this.hostname}`;
+        args.push("-p");    // hack for now
+        args.push("2222");
+        const target = `${this._user}@${this._hostname}`;
         args.push(target);
-        logger.debug(`Command: ${args}`);
-        return [...args];
+        logger.debug(`Command: ssh ${args}`);
+        return args;
     }
 
     getConfig(): Map<string, string> {
         // TODO: Will be useful for saving connections... future feature
         return new Map<string, string>;
+    }
+
+    getCommand(): string [] {
+        return this._command;
     }
 
 }
