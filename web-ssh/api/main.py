d462d368878f04f12b71fd191a0ea9da4c0458a2
Larry Banks <larrykbanks21@gmail.com>
2025-06-01 20:50:28 -0400

    cors

--- web-ssh/api/main.py
+++ web-ssh/api/main.py
@@ -4,6 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API", version="0.0")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get('/')
 def hello_world():
     return "Hello World!"
