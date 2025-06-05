d10a590c5110169ff0be7a4ef7152a99ea7a8347
Larry Banks <larrykbanks21@gmail.com>
2025-06-05 13:46:53 -0400

    preparing for rewrite of dashboard

--- web-ssh/web/src/pages/Dashboard.tsx
+++ web-ssh/web/src/pages/Dashboard.tsx
@@ -3,38 +3,20 @@ import { socket } from '../socket';
 import Terminal from '../components/Terminal';
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
-import ConnectionForm from "../components/ConnectionForm";
 import page from '../page.module.css'
 
 
 export default function Dashboard() {
-    // first need to enter a destination
-    // then need to establish a connection
+    // Get all connections from db and render
+    // when user clicks a connection, trigger a new ssh session
+        // eventually this will also have options to delete etc
     const [isLoading, setIsLoading] = useState(false);
-    const [sshConnectionData, setSshConnectionData] = useState<Record<string, string>>({
-        "user": "",
-        "pass": "",
-        "hostname": ""
-    });
     const [sessionStarted, setSessionStarted] = useState(false);
 
-    function updateSshConnectionField(field: string, value: string) {
-        setSshConnectionData({ ...sshConnectionData, [field]: value });
-        console.log(sshConnectionData);
-    }
-
     function startSession() {
-        for (const key in sshConnectionData) {
-            if (sshConnectionData[key] === "") {
-                console.warn(`${key} cannot be empty.`);
-                return;
-            }
-        }
+        
         setIsLoading(true);
         console.log(`Connecting with session credentials:\n`);
-        for (const key in sshConnectionData) {
-            console.log(`${key}:${sshConnectionData[key]}`);
-        }
         setSessionStarted(true);
         setIsLoading(false); // will be moved once sign-in is ironed out
         socket.on("disconnect", (reason) => {
@@ -54,7 +36,7 @@ export default function Dashboard() {
                 <Header />
                 <div className={page.page}>
                     <Sidebar />
-                    <Terminal sshConnectionData={sshConnectionData} sessionStarted={sessionStarted} />
+                    {/* <Terminal sshConnectionData={sshConnectionData} sessionStarted={sessionStarted} /> */}
                 </div>
             </div> 
         );
@@ -64,8 +46,8 @@ export default function Dashboard() {
         <div className={page.top_level}>
             <Header />
             <div className={page.page}>
-                <Sidebar />
-                <ConnectionForm startSession={startSession} updateSshConnectionField={updateSshConnectionField} />
+                {/* <Sidebar /> */}
+                Connections go here
             </div>
         </div>
     );
