c379915a265179aad36f9a2a2e7497ecc603fefb
Larry Banks <larrykbanks21@gmail.com>
2025-06-05 13:41:31 -0400

    New Connection button redirects to add connection page

--- web-ssh/web/src/components/Header.tsx
+++ web-ssh/web/src/components/Header.tsx
@@ -1,15 +1,18 @@
 import styles from '../styles/header.module.css'
 import buttons from '../styles/buttons.module.css'
+import { useNavigate } from "react-router";
 
 
 export default function Header() {
-    function handleClick() {
-        console.log("I've been clicked!");
-    }
+    let nav = useNavigate();
+
+    // change button to a link
     return (<div className={styles.header}>
         <span className={styles.title}>Alpha</span>
         <span className={styles.add_connections}>
-            <button className={buttons.default_button} onClick={handleClick}>New Connection</button>
+            <button className={buttons.default_button} onClick={() => {
+                nav("/add-connection");
+            }}>New Connection</button>
         </span>
     </div>);
 }
