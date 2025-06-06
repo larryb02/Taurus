54607d878d2c4673ee4c29b1084c5835b49969b3
Larry Banks <larrykbanks21@gmail.com>
2025-06-06 12:32:09 -0400

    custom context hook

---
+++ web-ssh/web/src/context/ConnectionContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+import type { Connection } from "../types";
+
+type Props = {
+  children: ReactNode;
+};
+
+interface ConnectionContextInterface {
+    connections: Connection[];
+    addConnection: (value: Connection) => void;
+}
+
+const ConnectionsContext = createContext<ConnectionContextInterface | null>(null);
+
+export const ConnectionsProvider: React.FC<Props> = ({ children }) => {
+    const [connections, setConnections] = useState<Connection[]>([
+        {
+            label: "test-server-1",
+            host: "testvm1",
+            user: "yo"
+        },
+        {
+            label: "prod-server-1",
+            host: "prodvm1",
+            user: "yo"
+        }]);
+        
+    function addConnection(conn: Connection) {
+        setConnections(old => [...old, conn]);
+    }
+
+    return (
+        <ConnectionsContext.Provider value={{connections, addConnection}}>
+            {children}
+        </ConnectionsContext.Provider>
+    );
+};
+
+export function useConnectionsContext() {
+    const connectionCtx = useContext(ConnectionsContext);
+    if (!connectionCtx) {
+        throw new Error(`ConnectionsContext must be used within a ConnectionsContext Provider`);
+    }
+    return connectionCtx;
+}
