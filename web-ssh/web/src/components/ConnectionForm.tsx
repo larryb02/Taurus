54967432a6c49e0af0021a4d11acf04fd568feaf
Larry Banks <larrykbanks21@gmail.com>
2025-06-06 14:44:05 -0400

    using context, changed props, and added label field

--- web-ssh/web/src/components/ConnectionForm.tsx
+++ web-ssh/web/src/components/ConnectionForm.tsx
@@ -1,35 +1,59 @@
-import styles from '../styles/ConnectionForm.module.css'
+import styles from '../styles/ConnectionForm.module.css';
+import { useConnectionsContext } from '../context/ConnectionContext';
+import { useState } from 'react';
 
 interface ConnectionFormProps {
-  createConnection: () => void;
-  updateSshConnectionField: (field: string, value: string) => void;
+    setIsAddingConnection: (value: boolean) => void;
 }
 
-export default function ConnectionForm({ 
-    createConnection, 
-    updateSshConnectionField 
-} : ConnectionFormProps) {
+export default function ConnectionForm({
+    setIsAddingConnection
+}: ConnectionFormProps) {
+    const { addConnection } = useConnectionsContext();
+
+    const [connectionData, setConnectionData] = useState<Record<string, string>>({
+        "label": "",
+        "user": "",
+        "pass": "",
+        "hostname": ""
+    });
+
+    function updateConnectionField(field: string, value: string) {
+        setConnectionData({ ...connectionData, [field]: value });
+        console.log(connectionData);
+    }
     return <div className={styles.connection_form}>
+        <div className={styles.connection_form_item}>
+            <label>Label</label>
+            <input type="text" onChange={(e) => {
+                updateConnectionField("label", e.target.value);
+            }}></input>
+        </div>
         <div className={styles.connection_form_item}>
             <label>Username</label>
             <input type="text" onChange={(e) => {
-                updateSshConnectionField("user", e.target.value);
+                updateConnectionField("user", e.target.value);
             }}></input>
         </div>
         <div className={styles.connection_form_item}>
             <label>Hostname</label>
             <input type="text" onChange={(e) => {
-                updateSshConnectionField("hostname", e.target.value);
+                updateConnectionField("hostname", e.target.value);
             }}></input>
         </div>
         <div className={styles.connection_form_item}>
             <label>Password</label>
             <input type="password" onChange={(e) => {
-                updateSshConnectionField("pass", e.target.value);
+                updateConnectionField("pass", e.target.value);
             }}></input>
         </div>
         <div className={styles.connection_form_item}>
-            <button onClick={createConnection}>Submit</button>
+            <button onClick={() => {
+                console.log("Connection created");
+                addConnection({ label: connectionData.label, host: connectionData.hostname, user: connectionData.user });
+                // should also store in db
+                setIsAddingConnection(false);
+            }}>Submit</button>
         </div>
     </div>;
 }
