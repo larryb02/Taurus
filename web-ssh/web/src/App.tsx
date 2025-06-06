fee73a7cd908ba3137c952adc5520fb51bea6a0e
Larry Banks <larrykbanks21@gmail.com>
2025-06-06 14:46:19 -0400

    Connection context provider at root level for now

--- web-ssh/web/src/App.tsx
+++ web-ssh/web/src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import AddConnection from "./pages/AddConnection";
+import { ConnectionsProvider } from "./context/ConnectionContext"; // DO NOT LEAVE THIS HERE
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="add-connection" element={<AddConnection />} />
-      </Routes>
-    </Router>
+    <ConnectionsProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </ConnectionsProvider>
   );
 }
 
