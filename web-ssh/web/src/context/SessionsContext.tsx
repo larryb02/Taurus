02e6c78443b3bf6ac50bced1d3dfc315dbc577e5
Larry Banks <larrykbanks21@gmail.com>
2025-06-07 16:14:04 -0400

    created context to store active session/s

---
+++ web-ssh/web/src/context/SessionsContext.tsx
@@ -0,0 +1,40 @@
+import { useState, createContext, useContext, type ReactNode } from "react";
+import { type Connection } from "../types";
+
+type Props = {
+    children: ReactNode;
+};
+
+interface SessionsContextInterface {
+    getActiveSession: () => Connection | null;
+    setActiveSession: (value: Connection) => void;
+}
+
+const SessionsContext = createContext<SessionsContextInterface | null>(null);
+
+export const SessionsContextProvider: React.FC<Props> = ({ children }) => {
+    const [activeSession, setActiveSession] = useState<Connection | null>(null);
+
+    function getActiveSession(): Connection | null { // i definitely need to test this.
+        if (!activeSession) {
+            throw new Error("Active Session is null");
+        }
+        return activeSession;
+    }
+    
+    return (
+        <SessionsContext value={{ getActiveSession, setActiveSession }}>
+            {children}
+        </SessionsContext>
+    );
+}
+
+export function useSessionsContext() {
+    const ctx = useContext(SessionsContext);
+
+    if (!ctx) {
+        throw new Error("SessionsContext must be used within a SessionsContext Provider");
+    }
+
+    return ctx;
+}
