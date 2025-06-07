179bfb5fd665d6ec4724e3ff224d94a6a90c2298
Larry Banks <larrykbanks21@gmail.com>
2025-06-07 16:23:59 -0400

    added sessions context provider to root

--- web-ssh/web/src/App.tsx
+++ web-ssh/web/src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import { ConnectionsProvider } from "./context/ConnectionContext"; // DO NOT LEAVE THIS HERE
+import TerminalView from "./pages/TerminalView";
+import { ConnectionsContextProvider } from "./context/ConnectionsContext"; // DO NOT LEAVE THIS HERE, or at least scope it down more
+import { SessionsContextProvider } from "./context/SessionsContext";
 
 function App() {
   return (
-    <ConnectionsProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </ConnectionsProvider>
+    <ConnectionsContextProvider>
+      <SessionsContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="session" element={<TerminalView />} />
+          </Routes>
+        </Router>
+        </SessionsContextProvider>
+    </ConnectionsContextProvider>
   );
 }
 
