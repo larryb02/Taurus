52e72070d737ef7653594199c4d7a0992eb3c076
Larry Banks <larrykbanks21@gmail.com>
2025-06-09 17:57:48 -0400

    added sidebar back

--- web-ssh/web/src/pages/TerminalView.tsx
+++ web-ssh/web/src/pages/TerminalView.tsx
@@ -1,7 +1,9 @@
 // This is where Terminals will be mounted, will be a window with tab functionality, split screen functionality, etc in the future
 // For now just dealing with one terminal
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 import page from '../page.module.css'; // these should probably be globals
+import termStyles from '../styles/terminal.module.css';
 import Terminal from '../components/Terminal'
 import { useSessionsContext } from '../context/SessionsContext';
 
@@ -11,22 +13,23 @@ export default function TerminalView() {
 
     const { getActiveSession } = useSessionsContext();
     const session = getActiveSession();
-    // if (!session) {
-    //     throw new Error("Active session is null"); // seems pretty hacky i will need to find a workaround, 
-    //                                                 // but in theory this **should** never happen.
-    // }
+
     const { label, user, host } = session;
     return (
         <div className={page.top_level}>
             <Header />
-            <div className="terminal-view">
-                <Terminal sshConnectionData={{
-                    "label": label,
-                    "user": user,
-                    "hostname": host,
-                    "pass": "password"
-                }} sessionStarted={true} />
+            <div className={page.page}>
+                <Sidebar />
+                <div className={termStyles.terminal_view}>
+                    <Terminal sshConnectionData={{
+                        "label": label,
+                        "user": user,
+                        "hostname": host,
+                        "pass": "password"
+                    }} sessionStarted={true} />
+                </div>
             </div>
+
         </div>
     );
 }
