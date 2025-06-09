a8cdd6a8dbe498e0083fe765e26cc4b1f6d1dcf5
Larry Banks <larrykbanks21@gmail.com>
2025-06-09 17:58:22 -0400

    added navlink to dashboard

--- web-ssh/web/src/components/Sidebar.tsx
+++ web-ssh/web/src/components/Sidebar.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import styles from '../styles/sidebar.module.css'
+import { NavLink } from "react-router-dom";
+// import { useSessionsContext } from "../context/SessionsContext";
 
 export default function Sidebar() {
-    const [recentConnections, setRecentConnections] = useState(['conn1', 'conn2']);
     const [isCollapsed, setIsCollapsed] = useState(false);
+    // const { getActiveSession } = useSessionsContext(); // will be used to render navlinks to active sessions in the future
 
     return (<div className={isCollapsed ? styles.sidebar_collapsed : styles.sidebar}>
         <div><button onClick={() => {
             setIsCollapsed(!isCollapsed);
             // console.log(isCollapsed)
         }}>Collapsable button</button></div>
-        {recentConnections.map((item) => 
-            <div>{item}</div>
-        )}
+        <NavLink to='/dashboard'>Dashboard</NavLink>
     </div>);
 }
