8f4842b0e59e8bd17df7939884d3740f1d06ae24
Larry Banks <larrykbanks21@gmail.com>
2025-06-09 18:00:10 -0400

    fetching connections from 'db' on page load

--- web-ssh/web/src/pages/Dashboard.tsx
+++ web-ssh/web/src/pages/Dashboard.tsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-// import { socket } from '../socket';
+import { useEffect, useState } from "react";
 import buttons from '../styles/buttons.module.css'
-// import Terminal from '../components/Terminal';
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import page from '../page.module.css'
 import ConnectionForm from "../components/ConnectionForm";
 import ConnectionItem from "../components/ConnectionItem";
-import { useConnectionsContext } from "../context/ConnectionContext";
+import { useConnectionsContext } from "../context/ConnectionsContext";
+
 
 export default function Dashboard() {
     // Get all connections from db and render
@@ -15,8 +14,26 @@ export default function Dashboard() {
     // eventually this will also have options to delete etc
 
     const [isAddingConnection, setIsAddingConnection] = useState<boolean>(false); // add connection button
-    const { connections } = useConnectionsContext();
+    const { connections, setConnections } = useConnectionsContext();
 
+    useEffect(() => {
+        const API_HOST = "http://localhost:8000/api";
+        
+        (async () => {
+            try {
+                const res = await fetch(`${API_HOST}/ssh/connection`);
+                // console.log(res);
+                const json = await res.json();
+                setConnections(json["results"]);
+            } catch (error) {
+                throw new Error(`Failed to fetch connections ${error}`);
+            }
+        })();
+
+        return () => {
+
+        }
+    }, []);
     if (isAddingConnection) {
         // we'll do some css magic here -> like a nice animation to render a panel
         // but for now we'll do this
@@ -24,7 +41,7 @@ export default function Dashboard() {
             <div className={page.top_level}>
                 <Header />
                 {/* <ConnectionsProvider> */}
-                    <ConnectionForm setIsAddingConnection={setIsAddingConnection} />
+                <ConnectionForm setIsAddingConnection={setIsAddingConnection} />
                 {/* </ConnectionsProvider> */}
             </div>
         )
@@ -32,18 +49,18 @@ export default function Dashboard() {
 
     return (
         // <ConnectionsProvider>
-            <div className={page.top_level}>
-                <Header />
-                <button className={buttons.default_button} onClick={() => {
-                    setIsAddingConnection(true);
-                }}>New Connection</button>
-                <div className={page.page}>
-                    {/* <Sidebar /> */}
-                    {connections.map((item) =>
-                        <ConnectionItem connection={item}/>
-                    )}
-                </div>
+        <div className={page.top_level}>
+            <Header />
+            <button className={buttons.default_button} onClick={() => {
+                setIsAddingConnection(true);
+            }}>New Connection</button>
+            <div className={page.page}>
+                <Sidebar />
+                {connections.map((item) =>
+                    <ConnectionItem connection={item} />
+                )}
             </div>
+        </div>
         // </ConnectionsProvider>
     );
 }
