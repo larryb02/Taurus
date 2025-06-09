901a4fcb97feed12d850123df122976d6e0f9a94
Larry Banks <larrykbanks21@gmail.com>
2025-06-09 18:51:50 -0400

    sharing setConnections() as wel

--- web-ssh/web/src/context/ConnectionsContext.tsx
+++ web-ssh/web/src/context/ConnectionsContext.tsx
@@ -8,15 +8,13 @@ type Props = {
 interface ConnectionContextInterface {
     connections: Connection[];
     addConnection: (value: Connection) => void;
+    setConnections: (value: Connection[]) => void;
 }
 
 const ConnectionsContext = createContext<ConnectionContextInterface | null>(null);
 
 export const ConnectionsContextProvider: React.FC<Props> = ({ children }) => {
-    const [connections, setConnections] = useState<Connection[]>([
-            { label: "test-server-1", host: "testvm1", user: "yo" },
-            { label: "prod-server-1", host: "prodvm1", user: "yo" },
-        ]);
+    const [connections, setConnections] = useState<Connection[]>([]);
 
 
     function addConnection(conn: Connection) {
@@ -24,16 +22,16 @@ export const ConnectionsContextProvider: React.FC<Props> = ({ children }) => {
     }
 
     return (
-        <ConnectionsContext value={{ connections, addConnection }}>
+        <ConnectionsContext value={{ connections, addConnection, setConnections }}>
             {children}
         </ConnectionsContext>
     );
 };
 
-export function useConnectionsContext() {
-    const connectionCtx = useContext(ConnectionsContext);
-    if (!connectionCtx) {
+export const useConnectionsContext = () => {
+    const ctx = useContext(ConnectionsContext);
+    if (!ctx) {
         throw new Error(`ConnectionsContext must be used within a ConnectionsContext Provider`);
     }
-    return connectionCtx;
+    return ctx;
 }
