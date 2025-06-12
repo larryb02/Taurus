c2248f90647305cddf8b75c1a78b3663a6c70a9b
Larry Banks <larrykbanks21@gmail.com>
2025-06-12 15:37:00 -0400

    integrating database

---
+++ web-ssh/api/db/service.py
@@ -0,0 +1,35 @@
+from typing import Annotated
+from sqlalchemy import create_engine, URL
+from sqlalchemy.orm import Session
+from fastapi import Depends
+
+db_dialect = "postgresql"
+db_api = "psycopg2"
+db_host = "localhost"
+db_port = 5432
+db_user = "postgres"
+db_password = "example"
+db_db = "postgres"
+
+db_url = URL.create(
+    drivername=f"{db_dialect}+{db_api}",
+    username=db_user,
+    password=db_password,
+    host=db_host,
+    port=db_port,
+    database=db_db
+)
+
+engine = create_engine(db_url, echo=True)
+
+def get_session():
+    with Session(engine) as session:
+        # yield session
+        try:
+            yield session
+        except:
+            session.rollback()
+        finally:
+            session.close()
+
+DbSession = Annotated[Session, Depends(get_session)]
