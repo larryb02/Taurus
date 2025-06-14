62d480e4536ae4eb5e275f574906b4aa9b8acc89
Larry Banks <larrykbanks21@gmail.com>
2025-06-14 14:26:12 -0400

    function to get user record by id, renamed other get function for clarity

--- web-ssh/api/auth/service.py
+++ web-ssh/api/auth/service.py
@@ -6,28 +6,46 @@ from db.core import DbSession
 import bcrypt
 
 
-def get_current_user(request: Request):
-    if request.session.get("user_id"):
-        current_user = request.session.get("user_id")
-        return current_user
-    else:
-        print("No user signed in")
-        return None
-
-
-def get_user(user: User, dbsession: DbSession):
+def get_user_by_id(user_id: int, dbsession: DbSession):
     stmt = select(
         UserAccount.user_id,
         UserAccount.username,
         UserAccount.email_address,
         UserAccount.password,
         UserAccount.created_at,
-    ).where(user.email == UserAccount.email_address)
+    ).where(user_id == UserAccount.user_id)
     try:
         record = dbsession.execute(stmt).one_or_none()
         return record
     except Exception as e:
-        print(f"Exception in get_user: {e}")
+        print(f"Exception in get_user_by_id: {e}")
+        raise
+
+
+def get_current_user(request: Request):
+    print(request.session)
+    if request.session.get("user_id"):
+        current_user = request.session.get("user_id")
+        print(f"Current User: {current_user}")
+        return current_user
+    else:
+        print("No user signed in")
+        return None
+
+
+def get_user_by_email(email_address: str, dbsession: DbSession):
+    stmt = select(
+        UserAccount.user_id,
+        UserAccount.username,
+        UserAccount.email_address,
+        UserAccount.password,
+        UserAccount.created_at,
+    ).where(email_address == UserAccount.email_address)
+    try:
+        record = dbsession.execute(stmt).one_or_none()
+        return record
+    except Exception as e:
+        print(f"Exception in get_user_by_email: {e}")
         raise
 
 
@@ -36,15 +54,14 @@ def hash_password(password: str):
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(to_bytes, salt)
 
+
 def create_user(user: User, dbsession: DbSession) -> dict:
     # TODO validate input
 
     hashed_pw = hash_password(user.password)
     stmt = (
         insert(UserAccount)
-        .values(
-            email_address=user.email, username=user.username, password=hashed_pw
-        )
+        .values(email_address=user.email, username=user.username, password=hashed_pw)
         .returning(
             UserAccount.user_id,
             UserAccount.username,
