e6bfc51a4094ead0b482c8fb9bd6b503be3655ce
Larry Banks <larrykbanks21@gmail.com>
2025-06-13 20:03:09 -0400

    changed password type to bytea

--- web-ssh/api/auth/models.py
+++ web-ssh/api/auth/models.py
@@ -1,14 +1,21 @@
 from pydantic import BaseModel
 from db.core import Base
-from sqlalchemy import String, DATETIME
+from sqlalchemy import String, DATETIME, LargeBinary
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Optional
 from datetime import datetime
+
 
 class User(BaseModel):
     email: str
     username: str
     password: str
+
+
+class UserLogin(BaseModel):
+    email_or_user: str
+    password: str
+
 
 class UserAccount(Base):
     __tablename__ = "useraccount"
@@ -16,5 +23,5 @@ class UserAccount(Base):
     user_id: Mapped[int] = mapped_column(primary_key=True)
     email_address: Mapped[str] = mapped_column(String(100), unique=True)
     username: Mapped[str] = mapped_column(String(50))
-    password: Mapped[str] = mapped_column(String(50))
+    password: Mapped[str] = mapped_column(LargeBinary())
     created_at: Mapped[datetime] = mapped_column(DATETIME, default=datetime.now())
