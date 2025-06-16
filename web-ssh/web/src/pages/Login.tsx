d96051c960a2527a955c027090359c6061e153b8
Larry Banks <larrykbanks21@gmail.com>
2025-06-16 13:21:03 -0400

    redirect on succesful account creation

--- web-ssh/web/src/pages/Login.tsx
+++ web-ssh/web/src/pages/Login.tsx
@@ -56,17 +56,6 @@ export default function Login() {
 
             const data = await res.json();
             console.log(data);
-            // try {
-            //     const res = await fetch(`${config.api.url}${config.api.routes.auth.user}`, {
-            //         credentials: "include"
-            //     });
-            //     const json = await res.json();
-            //     console.log(json);
-            //     // return json;
-            // } catch (error) {
-            //     throw new Error(`Failed to fetch ${error}`);
-            // }
-
         } catch (error) {
             throw new Error(`Failed to fetch: {
                 Route: ${config.api.routes.auth.login}
@@ -83,12 +72,16 @@ export default function Login() {
             return;
         }
         try {
-            const res = await fetch(`${config.api.url}${config.api.routes.auth.create}`,
+            const res = await fetch(`${config.api.url}${config.api.routes.auth.user}`,
                 {
                     method: "POST",
-                    body: JSON.stringify(newUserData),
+                    body: JSON.stringify({
+                        "email": newUserData.emailAddress,
+                        "username": newUserData.username,
+                        "password": newUserData.password
+                    }),
                     headers: {
-                        "content-type": "application/json"
+                        "content-type": "application/json",
                     }
                 }
             );
@@ -98,13 +91,23 @@ export default function Login() {
                     Status Code: ${res.status}
                     Msg: ${res.statusText}}`);
             }
+            else {
+                console.log("Successfully created new account");
+                setNewUserData({
+                    emailAddress: "",
+                    username: "",
+                    password: "",
+                    confirmPassword: ""
+                });
+                console.log(userData)
+                setIsCreatingAccount(false);
+            }
 
-            const data = await res.json();
-            console.log(data);
+
 
         } catch (error) {
             throw new Error(`Failed to fetch: {
-                Route: ${config.api.routes.auth.create}
+                Route: ${config.api.routes.auth.user}
                 Error: ${error}}`
             );
         }
@@ -112,37 +115,40 @@ export default function Login() {
 
     if (isCreatingAccount) {
         return (
-            <div>
-                <button onClick={() => setIsCreatingAccount(false)}>Return</button>
-                <div className="login-field">
-                    {/* <label>Username</label> */}
-                    <input type="text" placeholder="Email" onChange={
-                        (e) => updateField(userData, setUserData, "email", e.target.value)
-                    }>
-                    </input>
+            <div className="login-page">
+                <div className="login-form">
+                    <button onClick={() => setIsCreatingAccount(false)}>Return</button>
+                    <div className="login-field">
+                        <input type="text" placeholder="Email Address" onChange={
+                            (e) => {
+                                updateField(newUserData, setNewUserData, "emailAddress", e.target.value)
+                                console.log(newUserData);
+                            }
+                        }>
+                        </input>
+                    </div>
+                    <div className="login-field">
+                        <input type="text" placeholder="Username" onChange={
+                            (e) => updateField(newUserData, setNewUserData, "username", e.target.value)
+                        }>
+                        </input>
+                    </div>
+                    <div className="login-field">
+                        <input type="password" placeholder="Password" onChange={
+                            (e) => updateField(newUserData, setNewUserData, "password", e.target.value)
+                        }>
+                        </input>
+                    </div>
+                    <div className="login-field">
+                        <input type="password" placeholder="Confirm Password" onChange={
+                            (e) => updateField(newUserData, setNewUserData, "confirmPassword", e.target.value)
+                        }>
+                        </input>
+                    </div>
+                    <div className="login-action">
+                        <button onClick={() => createAccount()}>Create</button>
+                    </div>
                 </div>
-                <div>
-                    <label>Username</label>
-                    <input type="text" onChange={
-                        (e) => updateField(newUserData, setNewUserData, "username", e.target.value)
-                    }>
-                    </input>
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" onChange={
-                        (e) => updateField(newUserData, setNewUserData, "password", e.target.value)
-                    }>
-                    </input>
-                </div>
-                <div>
-                    <label>Confirm Password</label>
-                    <input type="password" onChange={
-                        (e) => updateField(newUserData, setNewUserData, "confirmPassword", e.target.value)
-                    }>
-                    </input>
-                </div>
-                <button onClick={() => createAccount()}>Create</button>
             </div>
         );
     }
@@ -163,8 +169,14 @@ export default function Login() {
                     }>
                     </input>
                 </div>
-                <div>
-                    <button onClick={() => setIsCreatingAccount(true)}>Create Account</button>
+                <div className="login-action">
+                    {/* <button onClick={() => setIsCreatingAccount(true)}>Create Account</button> */}
+                    <a className="" href="#" onClick={(e) => {
+                        e.preventDefault(); // Prevent page jump
+                        setIsCreatingAccount(true);
+                    }}>
+                        Create Account
+                    </a>
                     <button onClick={() => signIn()}>Sign In</button>
                 </div>
             </div>
