dd8ce82ba4580ced391dec9f224fb6adf9a608ee
Larry Banks <larrykbanks21@gmail.com>
2025-06-18 15:34:14 -0400

    test case for get all route

--- tests/ssh_tests/integration.py
+++ tests/ssh_tests/integration.py
@@ -1,9 +1,10 @@
 from fastapi.testclient import TestClient
-
+import json
 from api.main import app
 
 client = TestClient(app)
 
+
 def test_get_all_connections():
-    response = client.get('api/ssh/connection')
+    response = client.get("api/ssh/connection")
     assert response.status_code == 200
