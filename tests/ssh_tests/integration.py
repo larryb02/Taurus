a2ef12058f0379dae1620997bf3c7a5074b2d166
Larry Banks <larrykbanks21@gmail.com>
2025-06-18 16:49:52 -0400

    sanity check

--- tests/ssh_tests/integration.py
+++ tests/ssh_tests/integration.py
@@ -8,3 +8,15 @@ client = TestClient(app)
 def test_get_all_connections():
     response = client.get("api/ssh/connection")
     assert response.status_code == 200
+    assert response.json() == json.loads(json.dumps(
+        {
+            "results": [
+                {
+                    "connection_id": 1,
+                    "label": "dummy_server",
+                    "hostname": "localhost",
+                    "username": "dummy",
+                }
+            ]
+        }
+    )) # yea fix this hacky mess
