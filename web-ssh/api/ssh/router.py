09cc0f034101b15e81d15ab581dcec8065c033a2
Larry Banks <larrykbanks21@gmail.com>
2025-06-19 16:56:54 -0400

    associated user_id of currently signed in user with each route

--- web-ssh/api/ssh/router.py
+++ web-ssh/api/ssh/router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 import json
 from .models import SSHConn
 from ..db.core import DbSession
 import logging
 from .service import get_all, create
+from ..auth.service import get_current_user, get_user_by_id
 
 ssh_logger = logging.getLogger("web_ssh.ssh")
 # ssh_logger.propagate = False
@@ -13,13 +14,19 @@ router = APIRouter(prefix="/ssh")
 
 
 @router.get("/connection")
-def get_all_ssh_connections(session: DbSession):
+def get_all_ssh_connections(session: DbSession, request: Request):
     """
     Get all ssh connections within 'user_id\'s' namespace
     """
+    user_id = get_current_user(request)
+    if not user_id:
+        raise HTTPException(
+            status_code=400, detail="Failed to process request, no user logged in"
+        )
     ssh_logger.info("Getting all connections")
     try:
-        connections = get_all(session)
+        user = get_user_by_id(user_id, session)
+        connections = get_all(user, session)
         ssh_logger.info(f"Got connections: {connections}")
         if len(connections) == 0:
             ssh_logger.debug("Connections [] is emtpy")
@@ -40,13 +47,21 @@ def get_all_ssh_connections(session: DbSession):
 
 
 @router.post("/connection")
-def create_connection(conn: SSHConn, session: DbSession):
+def create_connection(conn: SSHConn, session: DbSession, request: Request):
     """
     Create a ssh connection within 'user_id\'s' namespace
     """
+    user_id = get_current_user(request)
+    if not user_id:
+        raise HTTPException(
+            status_code=400, detail="Failed to process request, no user logged in"
+        )
     try:
-        create(conn, session)
+        # user = get_user_by_id(user_id, session)
+        ssh_logger.debug("Creating connection")
+        res = dict(create(user_id, conn, session)._mapping)
+        ssh_logger.debug("Session created")
+        return {"results": res}
     except Exception as e:
         ssh_logger.error(f"Failed to create user: {e}")
         raise HTTPException(status_code=400, detail="Failed to process request")
-    return {"results": "Created"}
--- web-ssh/api/ssh/service.py
+++ web-ssh/api/ssh/service.py
@@ -1,39 +1,68 @@
 from ..db.core import DbSession
 from .models import SSHConnection, SSHConn
-from sqlalchemy import select, insert
+from ..auth.models import UserAccount
+
+# from ..auth.service import get_current_user
+from sqlalchemy import select, insert, and_
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 import logging
+import pickle
+import os
 
 ssh_service_logger = logging.getLogger("web_ssh.ssh.service")
-ssh_service_logger.propagate = False
 
 
-def get_all(dbsession: DbSession):
+def get_all(user: UserAccount, dbsession: DbSession):
     stmt = select(
         SSHConnection.connection_id,
         SSHConnection.label,
         SSHConnection.hostname,
         SSHConnection.username,
     ).where(
-        SSHConnection.user_id == 1
+        SSHConnection.user_id == user.user_id
     )  # note need to either pass id in query string on client
     # or get it from session object
+    print(stmt)
     try:
         connections = list(dbsession.execute(stmt).all())
         return connections
     except Exception as e:
         ssh_service_logger.error(f"Failed to execute query {e}")
 
+def get():
+    # TODO: stub for getting connection, 
+    # will be needed to spin up ssh sessions
+    pass
 
-def create(ssh_connection: SSHConn, dbsession: DbSession):
-    
+def create(user_id: int, ssh_connection: SSHConn, dbsession: DbSession):
+    try:
+        credentials_as_bytes = pickle.dumps(ssh_connection.credentials)
+    except Exception as e:
+        ssh_service_logger.error(f"Failed to serialize {e}")
+    ssh_service_logger.debug(f"Creds as bytes: {credentials_as_bytes}")
+    key_file = open("secrets.key", "rb")
+    key = key_file.read(32)
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
+    ciphertext = nonce + aesgcm.encrypt(nonce, credentials_as_bytes, None)
     stmt = (
         insert(SSHConnection)
-        .values()
+        .values(
+            label=ssh_connection.label,
+            hostname=ssh_connection.hostname,
+            username=ssh_connection.username,
+            user_id=user_id,
+            credentials=ciphertext,
+        )
         .returning(
+            SSHConnection.label,
             SSHConnection.connection_id,
             SSHConnection.hostname,
             SSHConnection.created_at,
-            SSHConnection.label,
         )
     )
-    pass
+    try:
+        connection = dbsession.execute(stmt)
+        return connection.one_or_none()
+    except Exception as e:
+        ssh_service_logger.error(f"Failed to insert: {e}")
