f6d1bf5fb5d9d378491ed3695fea38122d30ec87
Larry Banks <larrykbanks21@gmail.com>
2025-06-20 03:28:08 -0400

    minimal validation

--- web-ssh/web/src/components/ConnectionForm.tsx
+++ web-ssh/web/src/components/ConnectionForm.tsx
@@ -21,24 +21,51 @@ export default function ConnectionForm({
         "hostname": ""
     });
 
+    const [error, setError] = useState<string | null>(null);
+
     // const updateConnectionField = (field: string, value: string) => {
     //     setConnectionData({ ...connectionData, [field]: value });
     //     console.log(connectionData);
     // }
 
-    const createNewConnection = async (conn: Record<string, string>) => {
-        console.log(`Adding new connection to db ${JSON.stringify(conn)}`);
+    const createNewConnection = async () => {
+        if (connectionData.label === "") {
+            setError("Label cannot be empty");
+            return;
+        }
+        if (connectionData.user === "") {
+            setError("User cannot be empty.");
+            return;
+        }
+        if (connectionData.hostname === "") {
+            setError("Hostname cannot be empty.");
+            return;
+        }
+        if (connectionData.password === "") {
+            setError("Password cannot be empty.");
+            return;
+        }
         try {
             let res = await fetch(`${config.api.url}${config.api.routes.ssh.connection}`, {
                 method: "POST",
-                body: JSON.stringify(conn),
+                body: JSON.stringify({
+                    label: connectionData.label,
+                    hostname: connectionData.hostname,
+                    username: connectionData.user,
+                    credentials: {
+                        auth_type: "password", // hardcoding is temporary
+                        credentials: connectionData.password
+                    }
+                }),
                 headers: {
                     "content-type": "application/json"
-                }
+                },
+                credentials: "include"
             });
 
             if (!res.ok) {
-                throw new Error(`Something went wrong ${res.status}, ${res.statusText}`)
+                setError("Something went wrong, please try again.");
+                throw new Error(`Failed to process request: ${res.status}, ${res.statusText}`)
             }
 
             res = await res.json();
@@ -50,7 +77,9 @@ export default function ConnectionForm({
             }
             );
             console.log(res);
+            setIsAddingConnection(false);
         } catch (error) {
+            setError("Something went wrong, please try again.");
             throw new Error(`Failed to add connection to db ${error}`);
         }
     }
@@ -82,13 +111,16 @@ export default function ConnectionForm({
                 updateField(connectionData, setConnectionData, "password", e.target.value);
             }}></input>
         </div>
+        {error !== null &&
+            <div className="error-msg">
+                {error}
+            </div>
+        }
         <div className={styles.connection_form_item}>
             <button onClick={() => {
-                console.log("Connection created");
                 // addConnection({ label: connectionData.label, host: connectionData.hostname, user: connectionData.user });
                 // should also store in db
-                createNewConnection(connectionData);
-                setIsAddingConnection(false);
+                createNewConnection();
             }}>Submit</button>
         </div>
     </div>;
