37814180fc25f17e62eb004569eec52c27119579
Larry Banks <larrykbanks21@gmail.com>
2025-06-20 11:04:45 -0400

    moved connection view to its own component

---
+++ web-ssh/web/src/components/ConnectionView.tsx
@@ -0,0 +1,53 @@
+import { config } from "../config";
+import { useEffect, useState } from "react";
+import { useConnectionsContext } from "../context/ConnectionsContext";
+import ConnectionItem from "./ConnectionItem";
+import Header from '../components/Header';
+import ConnectionForm from "../components/ConnectionForm";
+import buttons from '../styles/buttons.module.css';
+
+export default function ConnectionView() {
+    const [isAddingConnection, setIsAddingConnection] = useState<boolean>(false); // add connection button
+    const { connections, setConnections } = useConnectionsContext();
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                // NOTE: fetching connections after getting user works for now, 
+                // but consider moving this to a function in ConnectionsContext
+                const res = await fetch(`${config.api.url}${config.api.routes.ssh.connection}`, {
+                    credentials: "include"
+                });
+                const data = await res.json();
+                setConnections(data["results"]);
+            } catch (error) {
+                throw new Error(`Failed to fetch ${error}`);
+            }
+        }
+        fetchUser();
+    }, []);
+
+    if (isAddingConnection) {
+        // we'll do some css magic here -> like a nice animation to render a panel
+        // but for now we'll do this
+        return (
+            <div className="connections-view">
+                <ConnectionForm setIsAddingConnection={setIsAddingConnection} />
+            </div>
+        )
+    }
+
+    return <div className="connections-view"> {/* make this display: block */}
+        <div className="connections-header">
+            <div className="button-container">
+                <button className={`${buttons.default_button} new-connection-button`} onClick={() => {
+                    setIsAddingConnection(true);
+                }}>New Connection</button>
+            </div>
+        </div>
+        <div className="connection-list">
+            {connections.length > 0 ? connections.map((item) =>
+                <ConnectionItem connection={item} />
+            ) : <div>Create a connection!!</div>}
+        </div>
+    </div>;
+}
--- web-ssh/web/src/pages/Dashboard.tsx
+++ web-ssh/web/src/pages/Dashboard.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react";
-import buttons from '../styles/buttons.module.css'
-import Header from '../components/Header'
-import Sidebar from '../components/Sidebar'
-import page from '../page.module.css'
-import '../styles/Dashboard.css'
-import ConnectionForm from "../components/ConnectionForm";
-import ConnectionItem from "../components/ConnectionItem";
+import { useEffect } from "react";
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import '../styles/Dashboard.css';
+import ConnectionView from "../components/ConnectionView";
 import { useConnectionsContext } from "../context/ConnectionsContext";
 import { useUserContext } from "../context/UserContext";
 import { config } from "../config";
@@ -13,12 +10,8 @@ import { type User } from "../types";
 
 
 export default function Dashboard() {
-    // Get all connections from db and render
-    // when user clicks a connection, trigger a new ssh session
-    // eventually this will also have options to delete etc
 
-    const [isAddingConnection, setIsAddingConnection] = useState<boolean>(false); // add connection button
-    const { connections, setConnections } = useConnectionsContext();
+    const { setConnections } = useConnectionsContext();
     const { setCurrentUser } = useUserContext();
 
 
@@ -54,55 +47,14 @@ export default function Dashboard() {
     }, []);
 
 
-    // useEffect(() => {
-    //     console.log(`Starting Fetch: ${config.api.url}${config.api.routes.ssh.connection}`);
-    //     (async () => {
-    //         try {
-    //             const res = await fetch(`${config.api.url}${config.api.routes.ssh.connection}`);
-    //             // console.log(res);
-    //             const json = await res.json();
-    //             if (!res.ok) {
-    //                 throw new Error(`Failed to fetch ssh connections: ${res.status}, ${res.statusText}`);
-    //             }
-    //             setConnections(json["results"]);
-    //         } catch (error) {
-    //             throw new Error(`Failed to fetch connections ${error}`);
-    //         }
-    //     })();
-    //     console.log(`Fetch for ${config.api.url}${config.api.routes.ssh.connection} complete`)
-    //     return () => {
-    //     }
-    // }, []);
-
-
-
-    if (isAddingConnection) {
-        // we'll do some css magic here -> like a nice animation to render a panel
-        // but for now we'll do this
-        return (
-            <div className={page.top_level}>
-                <Header />
-                {/* <ConnectionsProvider> */}
-                <ConnectionForm setIsAddingConnection={setIsAddingConnection} />
-                {/* </ConnectionsProvider> */}
-            </div>
-        )
-    }
 
     return (
-        // <ConnectionsProvider>
         <div className="dashboard">
             <Header />
-            <button className={buttons.default_button} onClick={() => {
-                setIsAddingConnection(true);
-            }}>New Connection</button>
-            <div className={page.page}>
+            <div className="dashboard-view">
                 <Sidebar />
-                {connections.length > 0 ? connections.map((item) =>
-                    <ConnectionItem connection={item} />
-                ) : <div>Create a connection!!</div>}
+                <ConnectionView />
             </div>
         </div>
-        // </ConnectionsProvider>
     );
 }
