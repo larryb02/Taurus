4b733080ce7ae0d7df897a46afd74cde538f6191
Larry Banks <larrykbanks21@gmail.com>
2025-06-29 13:07:52 -0400

    separated terminal instance from ui code

--- web-ssh/web/src/Terminal/Terminal.tsx
+++ web-ssh/web/src/Terminal/Terminal.tsx
@@ -1,94 +1,36 @@
-"use client";
-import { Terminal } from '@xterm/xterm';
-import { FitAddon } from '@xterm/addon-fit';
-import '@xterm/xterm/css/xterm.css';
-import { useEffect, useState } from 'react';
-import { socket } from '@taurus/lib/socket';
+import Term from '@taurus/lib/Terminal/Terminal';
+import { useEffect, useState, useRef } from 'react';
 import '@taurus/styles/terminal.css';
 
 interface terminalProps {
     sshConnectionData: number;
 }
 
-export default function terminal({ sshConnectionData }: terminalProps) { // need to resolve naming conflicts...
-    /** 
-     * Step 1:
-     *  - Do not pass credentials from the clientside
-     *  - Establish a websocket connection and handle connection logic on server
-     *  - Register event listeners that signal successful or failed connection
-     * Step 2:
-     *  - Mount components
-     *      - Terminal + socket i/o events
-     * Step 3:
-     *  - Make it possible to render multiple terminals and switch between them
-     *  
-     *
-     */
+export default function SshSession({ sshConnectionData }: terminalProps) { // need to resolve naming conflicts...
     const [error, setError] = useState<string | null>(null);
+    const termRef = useRef<Term>(null);
 
-    let term: Terminal | null = null;
-    const openTerm = () => {
-        const fitAddon = new FitAddon();
-        const term = new Terminal();
-        term.loadAddon(fitAddon);
-        term.open(document.getElementById('terminal') as HTMLElement);
-        fitAddon.fit();
-    }
-    const init = () => {
-        // register bare minimum events for setting up connection
-        socket.on("connect", () => {
-            // send connection id
-            socket.emit("connection-info", sshConnectionData);
-        });
-
-        socket.on("success", () => {
-            // console.log(`Success!`);
-            // socket.removeListener("connect");
-            // setup term
-            const fitAddon = new FitAddon();
-            term = new Terminal();
-            term?.loadAddon(fitAddon);
-            if (document.getElementById('terminal')) {
-                term?.open(document.getElementById('terminal') as HTMLElement);
-                console.log(`Proposed Dimensions: ${JSON.stringify(fitAddon.proposeDimensions())}`);
-                fitAddon.fit();
-            }
-
-            term?.onData(data => {
-                console.log(`sending input event: ${JSON.stringify(data)}`);
-                socket.emit("terminal:input", data);
-            });
-
-            socket.on("pty:output", (chunk) => {
-                console.log("Received data from pty", chunk);
-                term?.write(chunk);
-            });
-
-            socket.on("sessionTerminated", () => {
-                socket.disconnect();
-                term?.dispose();
-            })
-
-        });
-        socket.on("err", () => {
-            console.log(`error`);
-            setError("Failed to establish connection");
-            socket.disconnect();
-        })
-        socket.on("disconnect", () => {
-            socket.off("status");
-            console.log(`Disconnected from socket`);
-        });
-        socket.connect();
-    }
     useEffect(() => {
-        openTerm(); // only for testing
-        // init();
+        // openTerm(); // only for testing
+        (async () => {
+            termRef.current = new Term({
+                connectionId: sshConnectionData,
+                el: document.getElementById('terminal')
+            }); // change sshConnectionData to connectionId
+            const status = await termRef.current?.createNewSession();
+
+            if (status === true) {
+                termRef.current?.open();
+            }
+            else {
+                console.error("Failed to open");
+                setError("Failed to establish connection");
+            }
+        })();
+
         return () => {
             console.log("Cleanup called");
-            socket.removeAllListeners();
-            socket.disconnect();
-            term?.dispose();
+            termRef.current?.close();
         };
     }, []);
 
@@ -97,9 +39,6 @@ export default function terminal({ sshConnectionData }: terminalProps) { // need
     }
 
     return (
-        // <div className="terminal-view">
-        // <div id="container">
         <div id="terminal"></div>
-        // </div>
     );
 }
--- web-ssh/web/src/lib/Terminal/Terminal.ts
+++ web-ssh/web/src/lib/Terminal/Terminal.ts
@@ -6,40 +6,84 @@ import type { Socket } from 'socket.io-client';
 
 export default class TerminalTTY { // Rename because of type conflicts
     private _term: Terminal;
-    private _el: HTMLElement | null = document.getElementById('terminal');
+    private _el: HTMLElement | null;
     private _socket: Socket = socket;
+    private _connectionId: number;
 
-    constructor() {
+    // will also add ITerminalOptions
+    constructor(opts: {
+        connectionId: number,
+        el: HTMLElement | null
+    }) {
         this._term = new Terminal();
-    } 
+        this._connectionId = opts.connectionId;
+        this._el = opts.el;
+    }
 
     // also need to add startup logic
+    createNewSession(): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+
+            this._socket.on("connect", () => {
+                // send connection id
+                this._socket.emit("connection-info", this._connectionId);
+            });
+
+            // Might switch to an event driven approach, but this works
+            // e.g. term.on('success') or term.on('failure)
+            this._socket.once("success", () => {
+                resolve(true);
+            });
+
+            this._socket.once("error", (msg) => {
+                reject(false);
+            });
+            try {
+                this._socket.connect();
+            } catch (error) {
+                console.error("Failed to connect to websocket")
+            }
+            // register bare minimum events for setting up connection
+
+        });
+    }
+    // also create enums for events
     open() {
         const fitAddon = new FitAddon();
         this._term.loadAddon(fitAddon);
-        if (this._el)
+        if (this._el) {
             this._term.open(this._el);
+        }
+        else {
+            this.close(); // this might fail because term was never opened
+            throw new Error(`Failed to open terminal on ${this._el}`)
+        }
 
         this._term.onData(data => {
+            console.log(`terminal.input ${data}`)
             this._socket.emit("terminal:input", data);
         });
 
         this._socket.on("pty:output", data => {
             // just write raw for now
+            console.log(`terminal.output ${data}`);
             this._term.write(data);
         });
 
         this._socket.on("sessionTerminated", () => {
+            console.log("terminal.terminated");
             this._socket.removeAllListeners();
             this._socket.disconnect();
-        })
+        });
 
         this._socket.on("err", () => {
             console.log("An error occurred at the socket level");
-        })
+        });
     }
 
     close() {
+        this._socket.removeAllListeners();
+        this._socket.disconnect();
         this._term.dispose();
     }
 }
