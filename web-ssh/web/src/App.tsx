b431af3e6b42977cfdfa0b9a321afe591553f569
Larry Banks <larrykbanks21@gmail.com>
2025-06-30 15:52:03 -0400

    route for signup

--- web-ssh/web/src/App.tsx
+++ web-ssh/web/src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Dashboard from "./Dashboard/Dashboard";
-import TerminalView from "./Terminal/TerminalView";
-import Login from './Login/Login'
-import { ConnectionsContextProvider } from "./Dashboard/Connections/ConnectionsContext"; // DO NOT LEAVE THIS HERE, or at least scope it down more
-import { SessionsContextProvider } from "./context/SessionsContext";
-import { UserContextProvider } from "./context/UserContext";
+import Dashboard from "@taurus/Dashboard/Dashboard";
+import TerminalView from "@taurus/Terminal/TerminalView";
+import Login from '@taurus/Login/Login';
+import SignUp from '@taurus/Login/SignUp';
+import { ConnectionsContextProvider } from "@taurus/Dashboard/Connections/ConnectionsContext"; // DO NOT LEAVE THIS HERE, or at least scope it down more
+import { SessionsContextProvider } from "@taurus/context/SessionsContext";
+import { UserContextProvider } from "@taurus/context/UserContext";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="session" element={<TerminalView />} />
               <Route path="login" element={<Login />} />
+              <Route path="signup" element={<SignUp />} />
             </Routes>
           </Router>
         </UserContextProvider>
