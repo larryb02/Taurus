4ec90ac7eadda78c8725533ad7882643f6ba6277
Larry Banks <larrykbanks21@gmail.com>
2025-06-30 14:18:08 -0400

    moved form to separate component and login logic is functional

--- web-ssh/web/src/Login/Login.tsx
+++ web-ssh/web/src/Login/Login.tsx
@@ -1,16 +1,7 @@
 import { useState } from "react";
-import { updateField } from "../utils";
-import { config } from "../config";
+import { updateField } from "@taurus/utils";
 import { useNavigate } from "react-router-dom";
-import Box from '@mui/material/Box';
-import { Button, Link } from "@mui/material";
-// import '../styles/Login.css';
-import { Stack, TextField, Typography } from "@mui/material";
-
-interface UserLoginForm {
-    email: string;
-    password: string;
-}
+import LoginForm from './LoginForm';
 
 interface NewUserForm {
     emailAddress: string;
@@ -21,10 +12,6 @@ interface NewUserForm {
 
 export default function Login() {
     let navigate = useNavigate();
-    const [userData, setUserData] = useState<UserLoginForm>({
-        email: "",
-        password: ""
-    });
 
     const [isCreatingAccount, setIsCreatingAccount] = useState<boolean>(false);
 
@@ -37,226 +24,133 @@ export default function Login() {
 
     const [error, setError] = useState<string | null>(null);
 
-    const signIn = async () => {
-        try {
-            const res = await fetch(`${config.api.url}${config.api.routes.auth.login}`,
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        "email_or_user": userData.email,
-                        "password": userData.password
-                    }),
-                    headers: {
-                        "content-type": "application/json"
-                    },
-                    credentials: "include"
-                }
-            );
-            if (!res.ok) {
-                const statusCode = res.status;
-                switch (statusCode) {
-                    case 403:
-                        setError("Incorrect username or password.");
-                        break;
-                    default:
-                        setError("Something went wrong, please try again.");
-                        break;
-                }
-                throw new Error(`Failed to fetch: {
-                    Status Code: ${res.status}
-                    Msg: ${res.statusText}}`);
-            }
-            const data = await res.json();
-            console.log(data);
-        } catch (error) {
-            setError("Internal Server Error, please try again.");
-            throw new Error(`Failed to fetch: {
-                Route: ${config.api.routes.auth.login}
-                Error: ${error}}`
-            );
-        }
+    // const signIn = async () => {
+    //     try {
+    //         const res = await fetch(`${config.api.url}${config.api.routes.auth.login}`,
+    //             {
+    //                 method: "POST",
+    //                 body: JSON.stringify({
+    //                     "email_or_user": userData.email,
+    //                     "password": userData.password
+    //                 }),
+    //                 headers: {
+    //                     "content-type": "application/json"
+    //                 },
+    //                 credentials: "include"
+    //             }
+    //         );
+    //         if (!res.ok) {
+    //             const statusCode = res.status;
+    //             switch (statusCode) {
+    //                 case 403:
+    //                     setError("Incorrect username or password.");
+    //                     break;
+    //                 default:
+    //                     setError("Something went wrong, please try again.");
+    //                     break;
+    //             }
+    //             throw new Error(`Failed to fetch: {
+    //                 Status Code: ${res.status}
+    //                 Msg: ${res.statusText}}`);
+    //         }
+    //         const data = await res.json();
+    //         console.log(data);
+    //     } catch (error) {
+    //         setError("Internal Server Error, please try again.");
+    //         throw new Error(`Failed to fetch: {
+    //             Route: ${config.api.routes.auth.login}
+    //             Error: ${error}}`
+    //         );
+    //     }
 
-        navigate("/dashboard");
-    }
+    //     navigate("/dashboard");
+    // }
 
-    const createAccount = async () => {
-        if (newUserData.password !== newUserData.confirmPassword) {
-            console.log("Passwords do not match.");
-            return;
-        }
-        try {
-            const res = await fetch(`${config.api.url}${config.api.routes.auth.user}`,
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        "email": newUserData.emailAddress,
-                        "username": newUserData.username,
-                        "password": newUserData.password
-                    }),
-                    headers: {
-                        "content-type": "application/json",
-                    }
-                }
-            );
-            if (!res.ok) {
-                setError("Failed to create user, please try again.");
-                throw new Error(`Failed to fetch: {
-                    Status Code: ${res.status}
-                    Msg: ${res.statusText}}`);
-            }
-            else {
-                console.log("Successfully created new account");
-                setNewUserData({
-                    emailAddress: "",
-                    username: "",
-                    password: "",
-                    confirmPassword: ""
-                });
-                console.log(userData)
-                setIsCreatingAccount(false);
-            }
-        } catch (error) {
-            throw new Error(`Failed to fetch: {
-                Route: ${config.api.routes.auth.user}
-                Error: ${error}}`
-            );
-        }
-    }
+    // const createAccount = async () => {
+    //     if (newUserData.password !== newUserData.confirmPassword) {
+    //         console.log("Passwords do not match.");
+    //         return;
+    //     }
+    //     try {
+    //         const res = await fetch(`${config.api.url}${config.api.routes.auth.user}`,
+    //             {
+    //                 method: "POST",
+    //                 body: JSON.stringify({
+    //                     "email": newUserData.emailAddress,
+    //                     "username": newUserData.username,
+    //                     "password": newUserData.password
+    //                 }),
+    //                 headers: {
+    //                     "content-type": "application/json",
+    //                 }
+    //             }
+    //         );
+    //         if (!res.ok) {
+    //             setError("Failed to create user, please try again.");
+    //             throw new Error(`Failed to fetch: {
+    //                 Status Code: ${res.status}
+    //                 Msg: ${res.statusText}}`);
+    //         }
+    //         else {
+    //             console.log("Successfully created new account");
+    //             setNewUserData({
+    //                 emailAddress: "",
+    //                 username: "",
+    //                 password: "",
+    //                 confirmPassword: ""
+    //             });
+    //             console.log(userData)
+    //             setIsCreatingAccount(false);
+    //         }
+    //     } catch (error) {
+    //         throw new Error(`Failed to fetch: {
+    //             Route: ${config.api.routes.auth.user}
+    //             Error: ${error}}`
+    //         );
+    //     }
+    // }
 
-    if (isCreatingAccount) {
-        return (
-            <div className="login-page">
-                <div className="login-form">
-                    <button onClick={() => setIsCreatingAccount(false)}>Return</button>
-                    <div className="login-field">
-                        <input type="text" placeholder="Email Address" onChange={
-                            (e) => {
-                                updateField(newUserData, setNewUserData, "emailAddress", e.target.value)
-                                console.log(newUserData);
-                            }
-                        }>
-                        </input>
-                    </div>
-                    <div className="login-field">
-                        <input type="text" placeholder="Username" onChange={
-                            (e) => updateField(newUserData, setNewUserData, "username", e.target.value)
-                        }>
-                        </input>
-                    </div>
-                    <div className="login-field">
-                        <input type="password" placeholder="Password" onChange={
-                            (e) => updateField(newUserData, setNewUserData, "password", e.target.value)
-                        }>
-                        </input>
-                    </div>
-                    <div className="login-field">
-                        <input type="password" placeholder="Confirm Password" onChange={
-                            (e) => updateField(newUserData, setNewUserData, "confirmPassword", e.target.value)
-                        }>
-                        </input>
-                    </div>
-                    <div className="login-action">
-                        <button onClick={() => createAccount()}>Create</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+    // if (isCreatingAccount) {
+    //     return (
+    //         <div className="login-page">
+    //             <div className="login-form">
+    //                 <button onClick={() => setIsCreatingAccount(false)}>Return</button>
+    //                 <div className="login-field">
+    //                     <input type="text" placeholder="Email Address" onChange={
+    //                         (e) => {
+    //                             updateField(newUserData, setNewUserData, "emailAddress", e.target.value)
+    //                             console.log(newUserData);
+    //                         }
+    //                     }>
+    //                     </input>
+    //                 </div>
+    //                 <div className="login-field">
+    //                     <input type="text" placeholder="Username" onChange={
+    //                         (e) => updateField(newUserData, setNewUserData, "username", e.target.value)
+    //                     }>
+    //                     </input>
+    //                 </div>
+    //                 <div className="login-field">
+    //                     <input type="password" placeholder="Password" onChange={
+    //                         (e) => updateField(newUserData, setNewUserData, "password", e.target.value)
+    //                     }>
+    //                     </input>
+    //                 </div>
+    //                 <div className="login-field">
+    //                     <input type="password" placeholder="Confirm Password" onChange={
+    //                         (e) => updateField(newUserData, setNewUserData, "confirmPassword", e.target.value)
+    //                     }>
+    //                     </input>
+    //                 </div>
+    //                 <div className="login-action">
+    //                     <button onClick={() => createAccount()}>Create</button>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     );
+    // }
     return (
-        <Box
-            sx={{
-                minHeight: '100vh',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                bgcolor: '#121212',
-                px: 2,
-                width: '100%'
-            }}
-        >
-            <Box
-                component="form"
-                // onSubmit={handleSubmit}
-                sx={{
-                    width: '100%',
-                    maxWidth: 400,
-                    bgcolor: '#1e1e1e',
-                    color: 'white',
-                    p: 4,
-                    borderRadius: 2,
-                    boxShadow: 4,
-                }}
-            >
-                <Stack spacing={2}>
-                    <Typography sx={{
-                        color: 'white'
-                    }} variant="h5" textAlign="center">
-                        Log In
-                    </Typography>
-
-                    {/* {error && <Alert severity="error">{error}</Alert>} */}
-
-                    <TextField
-                        sx={{
-                            color: 'white',
-                            bgcolor: 'white'
-                        }}
-                        size="small"
-                        label="Email"
-                        name="email"
-                        type="email"
-                        // value={form.email}
-                        // onChange={(e) => setForm({ ...form, email: e.target.value })}
-                        fullWidth
-                    // required
-                    />
-
-                    <TextField
-                        sx={{
-                            color: 'white',
-                            bgcolor: 'white'
-                        }}
-                        size="small"
-                        label="Password"
-                        name="password"
-                        type="password"
-                        // value={form.password}
-                        // onChange={(e) => setForm({ ...form, password: e.target.value })}
-                        fullWidth
-                    // required
-                    />
-                    <Link variant="subtitle2">Forgot password?</Link>
-                    <Button color="success" type="submit" variant="contained" fullWidth>
-                        Sign In
-                    </Button>
-                    <Typography variant="subtitle2">
-                        Don't have an account? <Link href='/signup'>Sign Up</Link>
-                    </Typography>
-                </Stack>
-            </Box>
-        </Box>
-        // <div className="login-page">
-        //     <div className="login-form">
-        //         <div className="login-field">
-        //             {/* <label>Username</label> */}
-        //             <input type="text" placeholder="Email Address" onChange={
-        //                 (e) => updateField(userData, setUserData, "email", e.target.value)
-        //             }>
-        //             </input>
-        //         </div>
-        //         <div className="login-field">
-        //             {/* <label>Password</label> */}
-        //             <input type="password" placeholder="Password" onChange={
-        //                 (e) => updateField(userData, setUserData, "password", e.target.value)
-        //             }>
-        //             </input>
-        //         </div>
-        //         {error !== null &&
-        //             <div className="error-msg">
-        //                 {error}
-        //             </div>
-        //         }
+        <LoginForm />
         //         <div className="login-action">
         //             {/* <button onClick={() => setIsCreatingAccount(true)}>Create Account</button> */}
         //             <a className="" href="#" onClick={(e) => {
---
+++ web-ssh/web/src/Login/LoginForm.tsx
@@ -0,0 +1,178 @@
+import { Button, Link, Stack, TextField, Typography, Container, Box, Alert } from "@mui/material";
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { updateField } from '@taurus/utils';
+import { config } from '@taurus/config';
+
+type UserLoginForm = {
+    email: string;
+    password: string;
+}
+const handleChange = () => {
+    // TODO: create a handler that can update the field that got changed
+}
+export default function LoginForm() {
+    const [userData, setUserData] = useState<UserLoginForm>({
+        email: "",
+        password: ""
+    });
+
+    const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
+    const validate = (input: UserLoginForm) => {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!input.email || !regex.test(input.email)) {
+            return false;
+        }
+        if (!input.password) { // don't have guidelines for strong password yet so no regex here
+            return false;
+        }
+        return true;
+    }
+
+    const signIn = async () => {
+        try {
+            const res = await fetch(`${config.api.url}${config.api.routes.auth.login}`,
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                        "email_or_user": userData.email,
+                        "password": userData.password
+                    }),
+                    headers: {
+                        "content-type": "application/json"
+                    },
+                    credentials: "include"
+                }
+            );
+            if (!res.ok) {
+                const statusCode = res.status;
+                switch (statusCode) {
+                    case 403:
+                        setError("Incorrect username or password.");
+                        break;
+                    default:
+                        setError("Something went wrong, please try again.");
+                        break;
+                }
+            }
+            else {
+                navigate("/dashboard");
+            }
+            const data = await res.json();
+            console.log(data);
+        } catch (error) {
+            setError("Internal Server Error. Please try again.");
+            throw new Error(`Failed to fetch: {
+                    Route: ${config.api.routes.auth.login}
+                    Error: ${error}}`
+            );
+        }
+
+
+    }
+
+    return (
+
+        <Container maxWidth={false} disableGutters
+            sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                bgcolor: '#121212',
+                px: 2,
+                maxWidth: '100vw'
+            }}>
+            <Box
+                action="?"
+                component="form"
+                // onSubmit={handleSubmit}
+                sx={{
+                    width: '100%',
+                    maxWidth: 400,
+                    bgcolor: '#1e1e1e',
+                    color: 'white',
+                    p: 4,
+                    borderRadius: 2,
+                    boxShadow: 4,
+                }}
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    if (!validate(userData)) {
+                        setError("Username or Password is invalid"); // okay make this more fine grained
+                        return;
+                    }
+                    signIn();
+                }}
+            >
+                <Stack spacing={2}>
+                    <Typography sx={{
+                        color: 'white'
+                    }} variant="h5" textAlign="center">
+                        Log In
+                    </Typography>
+
+                    {error && <Alert severity="error">{error}</Alert>}
+
+                    <TextField
+                        sx={{
+                            color: 'white',
+                            bgcolor: 'white'
+                        }}
+                        size="small"
+                        placeholder="Email Address"
+                        name="email"
+                        type="email"
+                        slotProps={{
+                            inputLabel: {
+                                shrink: true
+                            }
+                        }}
+                        onChange={(e) => {
+                            if (error) {
+                                setError(null);
+                            }
+                            updateField(userData, setUserData, "email", e.target.value)
+                        }}
+                        fullWidth
+                    // required
+                    />
+
+                    <TextField
+                        sx={{
+                            color: 'white',
+                            bgcolor: 'white'
+                        }}
+                        size="small"
+                        placeholder="Password"
+                        name="password"
+                        type="password"
+                        slotProps={{
+                            inputLabel: {
+                                shrink: true
+                            }
+                        }}
+                        onChange={(e) => {
+                            if (error) {
+                                setError(null);
+                            }
+                            updateField(userData, setUserData, "password", e.target.value)
+                        }}
+                        fullWidth
+                    // required
+                    />
+                    <Link variant="subtitle2">Forgot password?</Link>
+                    <Button color="success" type="submit" variant="contained" fullWidth>
+                        Sign In
+                    </Button>
+                    <Typography variant="subtitle2">
+                        Don't have an account? <Link href='/signup'>Sign Up</Link>
+                    </Typography>
+                </Stack>
+            </Box>
+        </Container>
+
+    )
+}
