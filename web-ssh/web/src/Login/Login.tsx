f37ccad16c613bca8e6ed94fb65880b73371278f
Larry Banks <larrykbanks21@gmail.com>
2025-06-30 11:38:24 -0400

    migrating login ui to mui, no logic done yet

--- web-ssh/web/src/Login/Login.tsx
+++ web-ssh/web/src/Login/Login.tsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import { updateField } from "../utils";
 import { config } from "../config";
 import { useNavigate } from "react-router-dom";
-import '../styles/Login.css';
+import Box from '@mui/material/Box';
+import { Button, Link } from "@mui/material";
+// import '../styles/Login.css';
+import { Stack, TextField, Typography } from "@mui/material";
 
 interface UserLoginForm {
     email: string;
@@ -160,38 +163,111 @@ export default function Login() {
         );
     }
     return (
-        <div className="login-page">
-            <div className="login-form">
-                <div className="login-field">
-                    {/* <label>Username</label> */}
-                    <input type="text" placeholder="Email Address" onChange={
-                        (e) => updateField(userData, setUserData, "email", e.target.value)
-                    }>
-                    </input>
-                </div>
-                <div className="login-field">
-                    {/* <label>Password</label> */}
-                    <input type="password" placeholder="Password" onChange={
-                        (e) => updateField(userData, setUserData, "password", e.target.value)
-                    }>
-                    </input>
-                </div>
-                {error !== null &&
-                    <div className="error-msg">
-                        {error}
-                    </div>
-                }
-                <div className="login-action">
-                    {/* <button onClick={() => setIsCreatingAccount(true)}>Create Account</button> */}
-                    <a className="" href="#" onClick={(e) => {
-                        e.preventDefault(); // Prevent page jump
-                        setIsCreatingAccount(true);
-                    }}>
-                        Create Account
-                    </a>
-                    <button onClick={() => signIn()}>Sign In</button>
-                </div>
-            </div>
-        </div>
+        <Box
+            sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                bgcolor: '#121212',
+                px: 2,
+                width: '100%'
+            }}
+        >
+            <Box
+                component="form"
+                // onSubmit={handleSubmit}
+                sx={{
+                    width: '100%',
+                    maxWidth: 400,
+                    bgcolor: '#1e1e1e',
+                    color: 'white',
+                    p: 4,
+                    borderRadius: 2,
+                    boxShadow: 4,
+                }}
+            >
+                <Stack spacing={2}>
+                    <Typography sx={{
+                        color: 'white'
+                    }} variant="h5" textAlign="center">
+                        Log In
+                    </Typography>
+
+                    {/* {error && <Alert severity="error">{error}</Alert>} */}
+
+                    <TextField
+                        sx={{
+                            color: 'white',
+                            bgcolor: 'white'
+                        }}
+                        size="small"
+                        label="Email"
+                        name="email"
+                        type="email"
+                        // value={form.email}
+                        // onChange={(e) => setForm({ ...form, email: e.target.value })}
+                        fullWidth
+                    // required
+                    />
+
+                    <TextField
+                        sx={{
+                            color: 'white',
+                            bgcolor: 'white'
+                        }}
+                        size="small"
+                        label="Password"
+                        name="password"
+                        type="password"
+                        // value={form.password}
+                        // onChange={(e) => setForm({ ...form, password: e.target.value })}
+                        fullWidth
+                    // required
+                    />
+                    <Link variant="subtitle2">Forgot password?</Link>
+                    <Button color="success" type="submit" variant="contained" fullWidth>
+                        Sign In
+                    </Button>
+                    <Typography variant="subtitle2">
+                        Don't have an account? <Link href='/signup'>Sign Up</Link>
+                    </Typography>
+                </Stack>
+            </Box>
+        </Box>
+        // <div className="login-page">
+        //     <div className="login-form">
+        //         <div className="login-field">
+        //             {/* <label>Username</label> */}
+        //             <input type="text" placeholder="Email Address" onChange={
+        //                 (e) => updateField(userData, setUserData, "email", e.target.value)
+        //             }>
+        //             </input>
+        //         </div>
+        //         <div className="login-field">
+        //             {/* <label>Password</label> */}
+        //             <input type="password" placeholder="Password" onChange={
+        //                 (e) => updateField(userData, setUserData, "password", e.target.value)
+        //             }>
+        //             </input>
+        //         </div>
+        //         {error !== null &&
+        //             <div className="error-msg">
+        //                 {error}
+        //             </div>
+        //         }
+        //         <div className="login-action">
+        //             {/* <button onClick={() => setIsCreatingAccount(true)}>Create Account</button> */}
+        //             <a className="" href="#" onClick={(e) => {
+        //                 e.preventDefault(); // Prevent page jump
+        //                 setIsCreatingAccount(true);
+        //             }}>
+        //                 Create Account
+        //             </a>
+        //             <button onClick={() => signIn()}>Sign In</button>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
