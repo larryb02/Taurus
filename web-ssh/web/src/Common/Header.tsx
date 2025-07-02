63032c9155850ea0c93ba5cf56ad32f8f4b5436a
Larry Banks <larrykbanks21@gmail.com>
2025-07-02 14:50:13 -0400

    removed useNav hook

--- web-ssh/web/src/Common/Header.tsx
+++ web-ssh/web/src/Common/Header.tsx
@@ -1,11 +1,9 @@
 import '@taurus/styles/header.css' // note this will no longer be a module
-import { useNavigate } from 'react-router-dom';
 import { useUserContext } from '@taurus/Auth/UserContext';
 import ProfileCard from './ProfileCard';
 
 export default function Header() {
     const { currentUser } = useUserContext();
-    const nav = useNavigate();
     return (<div className="header">
         <span className="">Alpha</span>
         <span className="">
